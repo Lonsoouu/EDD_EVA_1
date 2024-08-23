00755173ee846ad1064d009ac21c450bb65b030e
Lonsoouu <L23550354@chihuahua2.tecnm.mx>
2024-08-22 18:52:10 -0600

    Add files via upload

---
+++ EVA_1_Stack/src/eva_1_stack/EVA_1_Stack.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_stack;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA_1_Stack {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Inicia Main");
+        A();
+        System.out.println("Termina Main");
+        // TODO code application logic here
+    }
+    public static void A(){
+        System.out.println("Inicia Main");
+        B();
+        System.out.println("Termina Main");
+    }
+    public static void B(){
+        System.out.println("Inicia Main");
+        
+        System.out.println("Termina Main");
+    }
+}
