eb91bc9f202c4ed51b03cad77a308e09f4d7abb7
Lonsoouu <L23550354@chihuahua2.tecnm.mx>
2024-08-26 18:48:22 -0600

    Add files via upload

---
+++ EVA1_3_HEAP/build/classes/eva1_3_heap/EVA1_3_HEAP.rs
@@ -0,0 +1,2 @@
+eva1_3_heap.EVA1_3_HEAP
+eva1_3_heap.Prueba
---
+++ EVA1_3_HEAP/src/eva1_3_heap/EVA1_3_HEAP.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_heap;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_3_HEAP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i =5;
+        Prueba obj = new Prueba ();
+        Prueba obj2 = new Prueba ();
+        
+        System.out.println(i);
+        System.out.println(obj);
+        System.out.println(obj2);
+        obj = null; // Eliminamos 
+        
+    }
+    
+}
+class Prueba{
+    
+}
