64689a41ad8a5d111bf475fe617f3cd61f16055a
Lonsoouu <L23550354@chihuahua2.tecnm.mx>
2024-09-04 18:43:39 -0600

    Add files via upload

---
+++ EVA1_11_ARREGLOS_RAPIDEZ/src/eva1_11_arreglos_rapidez/EVA1_11_ARREGLOS_RAPIDEZ.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_11_arreglos_rapidez;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_11_ARREGLOS_RAPIDEZ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [] arreglo = new int [100];
+        for (int i = 0; i < arreglo.length; i++) {
+            arreglo [i] = (int)(Math.random()+ 100);
+            
+        }
+    }
+    
+}
